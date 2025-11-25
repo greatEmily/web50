7d2bf4289a0ce6aac5c6e08a648f26d5a88bac6a
Emily Duran <63298612+greatEmily@users.noreply.github.com>
2025-11-25 06:31:13 -0600

    Delete auctions directory

--- auctions/admin.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from .models import AuctionListing, Bid, Comment, Category
-
-admin.site.register(AuctionListing)
-admin.site.register(Bid)
-admin.site.register(Comment)
-admin.site.register(Category)
