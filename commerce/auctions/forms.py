261ef948a56dbbdf81e573a9ff8b92c94259b28f
Emily Duran <emilynbauer@outlook.com>
2025-11-26 08:36:59 -0600

    updating forms.py

--- commerce/auctions/forms.py
+++ commerce/auctions/forms.py
@@ -6,7 +6,7 @@ from .models import AuctionListing
 class AuctionListingForm(forms.ModelForm):
     class Meta:
         model = AuctionListing
-        fields = ['title', 'description', 'starting_bid', 'image', 'category', 'status']
+        fields = ['title', 'description', 'starting_bid', 'image', 'category']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
