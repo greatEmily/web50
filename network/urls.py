5a32c3e2a39701a5b860f880c590c57b3bc5ef69
Emily Duran <63298612+greatEmily@users.noreply.github.com>
2026-05-30 20:24:33 -0500

    Project 4 (cs50 web)

---
+++ network/urls.py
@@ -0,0 +1,18 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("new", views.new_post, name="new_post"),
+    path("", views.index, name="index"),
+    path("profile/<str:username>/", views.profile, name="profile"),
+    path("follow/<str:username>/", views.follow_toggle, name="follow_toggle"),
+    path("following", views.following, name="following"),
+    path("edit/<int:post_id>/", views.edit_post, name="edit_post"),
+    path("like/<int:post_id>/", views.like_toggle, name="like_toggle"),
+]
